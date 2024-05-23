d3931dcc7da1541f2ce8b3f713f982e9b1984d92
Volodymyr Petrenko <mailbox.github@gmail.com>
2024-05-23 14:46:51 +0300

    basic

--- notes/notes/urls.py
+++ notes/notes/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('noteapp.urls')),
+    path('users/', include('users.urls')),
 ]
 
